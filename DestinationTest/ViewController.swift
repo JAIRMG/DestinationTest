ee73d463ef5e446d128e94b286750dfe4d9b0606
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2019-04-28 21:15:13 -0500

    keychain test

--- DestinationTest/ViewController.swift
+++ DestinationTest/ViewController.swift
@@ -10,15 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let service = "myService"
+    let account = "myAccount"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         
+        //retrieving for group
+        
         let userDefaults = UserDefaults(suiteName: "group.com.mobilex")
         if let testUserId = userDefaults?.object(forKey: "DynamicVector") as? String {
             print("User Id: \(testUserId)")
+            
+            //saving in keychain
+            KeychainService.savePassword(service: service, account: account, data: testUserId)
+            
         }
+        
+        
+        
+        
+        
+        //retrieving from keychain
+        let pass = KeychainService.loadPassword(service: service, account: account)
+        print("Retrieve pass \(pass)")
+        
         
     }
 
